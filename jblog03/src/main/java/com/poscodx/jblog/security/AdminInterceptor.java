dc1ba548b722e77b7e8b28857aaabe76f3812aa4
YeJi222 <hhongyizi@gmail.com>
2023-10-07 12:35:41 +0900

    refact: code refactoring - get blogId in AdminInterceptor

--- jblog03/src/main/java/com/poscodx/jblog/security/AdminInterceptor.java
+++ jblog03/src/main/java/com/poscodx/jblog/security/AdminInterceptor.java
@@ -1,5 +1,8 @@
 package com.poscodx.jblog.security;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -16,10 +19,24 @@ public class AdminInterceptor implements HandlerInterceptor {
 			throws Exception {
 		
 		String requestURI = request.getRequestURI();
-    	String blogId = requestURI.split("/")[2];
-		
 		HttpSession session = request.getSession();
 		UserVo userVo = (UserVo) session.getAttribute("authUser");
+		
+		/* 정규 표현식 패턴
+		   'http://localhost:8080/jblog03/'와 그 다음 슬래시('/') 사이 [blogId] 세그먼트 추출
+			[^/] : '/' 문자를 제외(^ 부정을 의미)한 모든 문자와 매치한다는 의미
+			+ : 바로 앞의 패턴([^/]+)이 하나 이상의 문자를 나타내야 한다는 의미
+			즉, '/'가 아닌 문자가 하나 이상 나오는 경우를 추출
+			jblog03/와 / 사이에 오는 blogId를 추출할 수 있다
+		*/
+        Pattern pattern = Pattern.compile("/jblog03/([^/]+)/");
+        Matcher matcher = pattern.matcher(requestURI);
+
+        // 패턴과 일치하는 부분 찾기
+        String blogId = "";
+        if (matcher.find()) { 
+            blogId = matcher.group(1);
+        }
 		
 		if(userVo == null || !(userVo.getId().equals(blogId))) { // 권한 없는 경우 
 			System.out.println("자신의 블로그 관리자 페이지에만 들어갈 수 있습니다.");
