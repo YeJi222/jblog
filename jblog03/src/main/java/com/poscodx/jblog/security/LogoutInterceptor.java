38f4198525b8444147d1c790f1e6c0178916fafd
YeJi222 <hhongyizi@gmail.com>
2023-10-07 12:51:20 +0900

    refact: code refactoring - using regex in LogoutInterceptor

--- jblog03/src/main/java/com/poscodx/jblog/security/LogoutInterceptor.java
+++ jblog03/src/main/java/com/poscodx/jblog/security/LogoutInterceptor.java
@@ -2,6 +2,9 @@ package com.poscodx.jblog.security;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -9,19 +12,26 @@ import javax.servlet.http.HttpSession;
 public class LogoutInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-    	String requestURI = request.getRequestURI();
-    	String blogId = requestURI.split("/")[4];
-    	
-    	HttpSession session = request.getSession();
-    	session.removeAttribute("authUser");
-    	session.invalidate();
-
-    	if("main".equals(blogId)) { // jblog03 경로에서 로그아웃하는 경우 
-    		response.sendRedirect(request.getContextPath());
-    	} else { // 누군가의 블로그 경로에서 로그아웃하는 경우 
-    		response.sendRedirect(request.getContextPath() + "/" + blogId);
-    	}
-    	
-    	return false;
+		String requestURI = request.getRequestURI();
+		
+		Pattern pattern = Pattern.compile("/logout/([^/]+)");
+	    Matcher matcher = pattern.matcher(requestURI);
+	
+	    String blogId = "";
+	    if (matcher.find()) { 
+	        blogId = matcher.group(1);
+	    }
+		
+		HttpSession session = request.getSession();
+		session.removeAttribute("authUser");
+		session.invalidate();
+	
+		if("main".equals(blogId)) { // jblog03 경로에서 로그아웃하는 경우 
+			response.sendRedirect(request.getContextPath());
+		} else { // 누군가의 블로그 경로에서 로그아웃하는 경우 
+			response.sendRedirect(request.getContextPath() + "/" + blogId);
+		}
+		
+		return false;
     }
 }
